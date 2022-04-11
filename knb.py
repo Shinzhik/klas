33ccbf91c10af4c4e7af2b519bb796fce8472850
Aleksey Pavlov <pavlov.ap@students.dvfu.ru>
2022-04-11 12:49:02 +1000

    aleskey last part

---
+++ knb.py
@@ -0,0 +1,7 @@
+    else:
+            print('Вы проиграли :(')
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print('Поздравляем! Вы победили!')
+    else:
+        print('Вы проиграли :(')
